9b3b6a93ae7eaf89b6939660ce38d825424f6f4b
Rapidfire Computer Entertainment <rapidfirecomputerentertainment@gmail.com>
2023-11-28 09:47:05 -0500

    Starting Solara and the platform layer

---
+++ src/paranormal/platform.cxx
@@ -0,0 +1,6 @@
+// Copyright (C) Rapidfire Computer Entertainment.
+// Licensed under GNU LGPL - see LICENSE.txt for more.
+
+//=
+// Implements the Platform layer.
+//=
---
+++ src/paranormal/platform.hxx
@@ -0,0 +1,11 @@
+// Copyright (C) Rapidfire Computer Entertainment.
+// Licensed under GNU LGPL - see LICENSE.txt for more.
+
+//=
+// Interface to the Platform system.
+//=
+
+#ifndef PARA_PLATFORM_HEAD
+#define PARA_PLATFORM_HEAD
+
+#endif // PARA_PLATFORM_HEAD
