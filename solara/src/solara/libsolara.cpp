b4be6d96592633ca556d701fed5ecae598c09d9d
imphonic <rapidfirecomputerentertainment@gmail.com>
2023-12-08 00:11:01 -0500

    Switching focus to Solara (soon to be renamed)

---
+++ solara/src/solara/libsolara.cpp
@@ -0,0 +1,7 @@
+/**
+ * Copyright Rapidfire Computer Entertainment.
+ * Licensed under GNU LGPL - see LICENSE for details.
+ * @file: libsolara.cpp
+ * @brief: Library manifest for CMake.
+ * @author: Will Pimentel-Tonche
+**/
